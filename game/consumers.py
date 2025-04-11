062b2dc57796c12d6083f3b6ee770cef544806d2
Suprotiv <suprotiv3@gmail.com>
2025-04-11 09:39:50 +0530

    changes

--- game/consumers.py
+++ game/consumers.py
@@ -1,122 +1,150 @@
-import contextlib
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
-import random
-from game.helpers import *
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+from .helpers import checkWin, isDraw
 
-class GameConsumer(AsyncJsonWebsocketConsumer):
- 
-    board = {
-        0: '', 1: '', 2: '',
-        3: '', 4: '', 5: '',
-        6: '', 7: '', 8: '',
-    }
-    
+class GameConsumer(AsyncWebsocketConsumer):
+    rooms = {}  # Store room state
+
     async def connect(self):
-        print(self.scope['url_route']['kwargs']['id'])
         self.room_id = self.scope['url_route']['kwargs']['id']
-        self.group_name = f"group_{self.room_id}"
+        self.room_group_name = f'game_{self.room_id}'
 
-        with contextlib.suppress(KeyError):
-            if(len(self.channel_layer.groups[self.group_name]) >= 2):
-                await self.accept()
-                await self.send_json({
-                    "event": "show_error",
-                    "error": "This room is fool!! Just like you"
-                })
-                return await self.close(1)
-            
-        await self.accept()
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-
-        if(len(self.channel_layer.groups[self.group_name]) == 2):
-            tmpGroup = list(self.channel_layer.groups[self.group_name])
-            first_player = random.choice(tmpGroup)
-            tmpGroup.remove(first_player)
-            final_group = [first_player, tmpGroup[0]]
-            for i, channel_name in enumerate(final_group):
-                await self.channel_layer.send(channel_name, {
-                    "type": "gameData.send",
-                    "data": {
-                        "event": "game_start",
-                        "board": self.board,
-                        "myTurn": True if i==0 else False
-                    }
-                })
-
-    
-
-    
-    async def receive_json(self, content, **kwargs):
-        print(content)
-        if(content['event'] == "boardData_send"):
-    
-            winner = checkWin(content['board'])
-            if(winner):
-                return await self.channel_layer.group_send(self.group_name, {
-                    "type": "gameData.send",
-                    "data": {
-                        "event": "won",
-                        "board": content['board'],
-                        "winner": winner,
-                        "myTurn": False,
-                    }
-                })
-            elif(isDraw(content['board'])):
-                return await self.channel_layer.group_send(self.group_name, {
-                    "type": "gameData.send",
-                    "data": {
-                        "event": "draw",
-                        "board": content['board'],
-                        "myTurn": False,
-                    }
-                })
-            else:
-            
-                for channel_name in self.channel_layer.groups[self.group_name]:
-                    await self.channel_layer.send(channel_name, {
-                        "type": "gameData.send",
-                        "data": {
-                            "event": "boardData_send",
-                            "board": content['board'],
-                            "myTurn": False if self.channel_name==channel_name else True
-                        }
-                    })
-             
-        elif(content['event'] == "restart"):
-
-            if(len(self.channel_layer.groups[self.group_name]) == 2):
-                tmpGroup = list(self.channel_layer.groups[self.group_name])
-                first_player = random.choice(tmpGroup)
-                tmpGroup.remove(first_player)
-                final_group = [first_player, tmpGroup[0]]
-                for i, channel_name in enumerate(final_group):
-                    await self.channel_layer.send(channel_name, {
-                        "type": "gameData.send",
-                        "data": {
-                            "event": "game_start",
-                            "board": self.board,
-                            "myTurn": True if i==0 else False
-                        }
-                    })
-
- 
-    async def disconnect(self, code):
-        if(code==1):
-            return 
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
-        await self.channel_layer.group_send(self.group_name, {
-            "type": "gameData.send",
-            "data": {
-                "event": "opponent_left",
-                "board": self.board,
-                "myTurn": False,
+        # Initialize room if doesn't exist
+        if self.room_id not in self.rooms:
+            self.rooms[self.room_id] = {
+                'players': [],
+                'board': {str(i): '' for i in range(9)},
+                'current_turn': 'X'
             }
-        })
-        
 
-     
-    async def gameData_send(self, context):
-        await self.send_json(context['data'])
-        
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
 
-     
+        # Add player to room
+        if len(self.rooms[self.room_id]['players']) < 2:
+            self.rooms[self.room_id]['players'].append(self.channel_name)
+            if len(self.rooms[self.room_id]['players']) == 2:
+                # Start game when second player joins
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'game_start',
+                        'board': self.rooms[self.room_id]['board']
+                    }
+                )
+        else:
+            await self.send(json.dumps({
+                'event': 'show_error',
+                'error': 'Room is full'
+            }))
+            await self.close()
+
+    async def disconnect(self, close_code):
+        if self.room_id in self.rooms:
+            if self.channel_name in self.rooms[self.room_id]['players']:
+                self.rooms[self.room_id]['players'].remove(self.channel_name)
+                # Reset room state
+                self.rooms[self.room_id]['board'] = {str(i): '' for i in range(9)}
+                self.rooms[self.room_id]['current_turn'] = 'X'
+                # Notify other player
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'opponent_left',
+                    }
+                )
+
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        room = self.rooms[self.room_id]
+
+        if data['event'] == 'boardData_send':
+            room['board'] = data['board']
+            winner = checkWin(room['board'])
+            
+            if winner:
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'game_won',
+                        'winner': winner,
+                        'board': room['board']
+                    }
+                )
+            elif isDraw(room['board']):
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'game_draw',
+                        'board': room['board']
+                    }
+                )
+            else:
+                room['current_turn'] = 'O' if room['current_turn'] == 'X' else 'X'
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'board_update',
+                        'board': room['board']
+                    }
+                )
+        
+        elif data['event'] == 'restart':
+            room['board'] = {str(i): '' for i in range(9)}
+            room['current_turn'] = 'X'
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'game_start',
+                    'board': room['board']
+                }
+            )
+
+    async def game_start(self, event):
+        is_first_player = self.channel_name == self.rooms[self.room_id]['players'][0]
+        await self.send(json.dumps({
+            'event': 'game_start',
+            'board': event['board'],
+            'myTurn': is_first_player
+        }))
+
+    async def board_update(self, event):
+        is_my_turn = (self.rooms[self.room_id]['current_turn'] == 'X' and 
+                     self.channel_name == self.rooms[self.room_id]['players'][0]) or \
+                    (self.rooms[self.room_id]['current_turn'] == 'O' and 
+                     self.channel_name == self.rooms[self.room_id]['players'][1])
+        await self.send(json.dumps({
+            'event': 'boardData_send',
+            'board': event['board'],
+            'myTurn': is_my_turn
+        }))
+
+    async def game_won(self, event):
+        await self.send(json.dumps({
+            'event': 'won',
+            'winner': event['winner'],
+            'board': event['board']
+        }))
+
+    async def game_draw(self, event):
+        await self.send(json.dumps({
+            'event': 'draw',
+            'board': event['board']
+        }))
+
+    async def opponent_left(self, event):
+        await self.send(json.dumps({
+            'event': 'opponent_left',
+            'board': self.rooms[self.room_id]['board']
+        }))
+
+
